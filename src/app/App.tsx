067fbe157fa5853c3cb800e636421740008a4214
ovenoboyo <ovenoboyo@gmail.com>
2021-11-23 20:00:52 +0530

    Add styled components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
 import '../styles/app.css'
 import './i18n/app.i18n'
 
 export default function App() {
   const { t } = useTranslation('translation')
-  return <div>{t('test')}</div>
+  return (
+    <div>
+      <Box>{t('test')}</Box>
+    </div>
+  )
 }
+
+const Box = styled.div`
+  background: #000000;
+  border-radius: 16px;
+  width: 100px;
+  height: 100px;
+  padding: 50px;
+  text-align: center;
+`
